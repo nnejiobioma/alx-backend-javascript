6dd6123e8aee6907b3a72c4d25e456ea072ccb27
obiomanneji <nnejiobioma@gmail.com>
2023-10-03 17:12:54 +0100

    made changes to task eight

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,21 +1,21 @@
-export default function cleanSet(inputSet, startString) {
+export default function cleanSet(inSet, startString) {
     // Check if the inputSet is not a Set or startString is not a string
-    if (!(inputSet instanceof Set) || typeof startString !== 'string') {
+    if (!(inSet instanceof Set) || typeof startString !== 'string') {
         return '';
     }
 
     const parts = [];
 
     // Iterate through the values of the Set
-    for (const value of inputSet.values()) {
+    for (const value of inSet.values()) {
         // Check if the value is a string and starts with the specified startString
         if (typeof value === 'string' && value.startsWith(startString)) {
             // Extract the substring after startString
-            const valueSubStr = value.substring(startString.length);
+            const subStr = value.substring(startString.length);
 
             // Check if there is a non-empty substring to add
-            if (valueSubStr) {
-                parts.push(valueSubStr);
+            if (subStr) {
+                parts.push(subStr);
             }
         }
     }
