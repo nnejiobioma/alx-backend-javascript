a89bbd770a8d7ef17c400007199977b3240fc11b
obiomanneji <nnejiobioma@gmail.com>
2023-10-03 14:55:23 +0100

    task amended again

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,6 +1,9 @@
-function getStudentsByLocation(studentList, city) {
-    // Use the filter function to select students in the specified city
-    return studentsInCity = studentList.filter(student => student.location === city);
-}
+export default (students, city) => students.filter(student => student.location === city);
 
-export default getStudentsByLocation;
+
+// function getStudentsByLocation(studentList, city) {
+//     // Use the filter function to select students in the specified city
+//     return studentsInCity = studentList.filter(student => student.location === city);
+// }
+
+// export default getStudentsByLocation;
