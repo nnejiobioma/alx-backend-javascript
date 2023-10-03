2b7ff756f7c87350b84e876497d9509f596b58b1
obiomanneji <nnejiobioma@gmail.com>
2023-10-03 14:58:02 +0100

    made a test file for user folder

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,4 +1,21 @@
-export default (students, city) => students.filter(student => student.location === city);
+// Define a function named getStudentsByLocation
+function getStudentsByLocation(students, city) {
+    // Use the filter method to create a new array
+    // Filter the students based on their location
+    const filteredStudents = students.filter((student) => {
+        // Check if the student's location matches the specified city
+        return student.location === city;
+    });
+
+    // Return the filtered array of students
+    return filteredStudents;
+}
+
+// Export the getStudentsByLocation function as the default export
+export default getStudentsByLocation;
+
+
+//export default (students, city) => students.filter(student => student.location === city);
 
 
 // function getStudentsByLocation(studentList, city) {
