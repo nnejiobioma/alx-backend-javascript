c7bab42118f47a0376a8d908fccb8cbb20212617
nnejiobioma <nneji.obioma@gmail.com>
2023-11-24 12:12:43 +0000

    task four handled

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,31 @@
+const sinon = require('sinon');
+const chai = require('chai');
+const expect = chai.expect;
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  let stub;
+
+  beforeEach(() => {
+    stub = sinon.stub(Utils, 'calculateNumber').returns(10);
+  });
+
+  afterEach(() => {
+    stub.restore();
+  });
+
+  it('sendPaymentRequestToApi uses the calculateNumber method of Utils', () => {
+    const spy = sinon.spy(console, 'log');
+
+    sendPaymentRequestToApi(100, 20);
+
+    sinon.assert.calledOnce(stub);
+    sinon.assert.calledWith(stub, 'SUM', 100, 20);
+
+    sinon.assert.calledOnce(spy);
+    sinon.assert.calledWith(spy, 'The total is: 10');
+
+    spy.restore();
+  });
+});
